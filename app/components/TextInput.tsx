80e817daa3602b88cc2d844195a088121557a98e
Bruno Azzi <bruno.azzi@icloud.com>
2023-08-16 22:42:36 -0300

    fix: working login page

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -1,10 +1,15 @@
-export const TextInput = ({
-  label,
-  placeholder,
-}: {
+import type { InputHTMLAttributes } from "react";
+import { forwardRef } from "react";
+
+type Props = {
   label: string;
   placeholder?: string;
-}) => {
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export const TextInput = forwardRef<HTMLInputElement, Props>(function Input(
+  { label, placeholder, ...props }: Props,
+  ref
+) {
   return (
     <div className="relative mt-4">
       <label
@@ -14,12 +19,14 @@ export const TextInput = ({
         {label}
       </label>
       <input
+        ref={ref}
         type="text"
         name={label}
         id={label}
         className=" block w-80 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         placeholder={placeholder}
+        {...props}
       />
     </div>
   );
-};
+});
