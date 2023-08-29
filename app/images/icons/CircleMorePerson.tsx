605a22e2952c7cd95d410824aba3e95d52b1105d
Mauro Sorgatto <mauro.sorgatto@azzero.tec.br>
2023-08-29 18:24:31 -0300

    feat: Acesso rapido do dashboard com icons exportados com o circulops

---
+++ app/images/icons/CircleMorePerson.tsx
@@ -0,0 +1,63 @@
+export const CircleMorePerson = () => {
+  return (
+    <svg
+      width="50"
+      height="50"
+      viewBox="0 0 50 50"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_1696_1497)">
+        <rect x="2" y="1" width="46" height="46" rx="23" fill="#606875" />
+        <path
+          d="M25 19C25 20.6569 23.6569 22 22 22C20.3431 22 19 20.6569 19 19C19 17.3431 20.3431 16 22 16C23.6569 16 25 17.3431 25 19Z"
+          fill="white"
+        />
+        <path
+          d="M16.6153 30.428C16.218 30.1736 15.9878 29.721 16.046 29.2529C16.4142 26.292 18.9394 24 21.9999 24C25.0604 24 27.5856 26.2914 27.9537 29.2522C28.012 29.7203 27.7818 30.1729 27.3845 30.4273C25.8302 31.4225 23.9824 32 21.9999 32C20.0174 32 18.1696 31.4231 16.6153 30.428Z"
+          fill="white"
+        />
+        <path
+          d="M31.25 19.75C31.25 19.3358 30.9142 19 30.5 19C30.0858 19 29.75 19.3358 29.75 19.75V21.75H27.75C27.3358 21.75 27 22.0858 27 22.5C27 22.9142 27.3358 23.25 27.75 23.25H29.75V25.25C29.75 25.6642 30.0858 26 30.5 26C30.9142 26 31.25 25.6642 31.25 25.25V23.25H33.25C33.6642 23.25 34 22.9142 34 22.5C34 22.0858 33.6642 21.75 33.25 21.75H31.25V19.75Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_1696_1497"
+          x="0"
+          y="0"
+          width="50"
+          height="50"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="1" />
+          <feGaussianBlur stdDeviation="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_1696_1497"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_1696_1497"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
