d0056bdda8557fa95d2d5f2a83421038c8a40c47
Bruno Azzi <bruno.azzi@icloud.com>
2023-10-20 18:56:01 -0300

    feat: remove unused code

--- app/models/client.server.ts
+++ app/models/client.server.ts
@@ -8,45 +8,6 @@ const clientListData = Prisma.validator<Prisma.ClientArgs>()({
 
 export type ClientListItem = Prisma.ClientGetPayload<typeof clientListData>;
 
-export const Clients: ClientListItem[] = [
-  {
-    name: "Jane Cooper",
-    cpf: "090.023.098-34",
-    personalContact: "+55 (48) 9 9981-2874",
-    province: "SC",
-  },
-  {
-    name: "Cody Fisher",
-    cpf: "090.023.098-33",
-    personalContact: "cody.fisher@example.com",
-    province: "Owner",
-  },
-  {
-    name: "Esther Howard",
-    cpf: "090.023.098-32",
-    personalContact: "esther.howard@example.com",
-    province: "Member",
-  },
-  {
-    name: "Jenny Wilson",
-    cpf: "090.023.098-31",
-    personalContact: "jenny.wilson@example.com",
-    province: "Member",
-  },
-  {
-    name: "Kristin Watson",
-    cpf: "090.023.098-35",
-    personalContact: " kristin.watson@example.com",
-    province: "Admin",
-  },
-  {
-    name: "Cameron Williamson",
-    cpf: "090.023.098-36",
-    personalContact: "cameron.williamson@example.com",
-    province: "Member",
-  },
-];
-
 export const getClients = () => prisma.client.findMany();
 
 export const createClient = async (data: {
