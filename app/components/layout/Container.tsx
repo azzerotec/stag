0bd08e9de98e02ff399f4f1416ce5551f2bbea93
Bruno Azzi <bruno.azzi@icloud.com>
2023-10-21 20:09:37 -0300

    fix: container not growing

--- app/components/layout/Container.tsx
+++ app/components/layout/Container.tsx
@@ -2,5 +2,5 @@ import type { PropsWithChildren } from "react";
 import { Coluna } from "~/components/layout/Flex";
 
 export const Container = ({ children }: PropsWithChildren) => (
-  <Coluna className="px-4 py-10 sm:px-6 lg:px-8">{children}</Coluna>
+  <Coluna className="grow px-4 py-10 sm:px-6 lg:px-8">{children}</Coluna>
 );
